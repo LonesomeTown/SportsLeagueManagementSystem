51c5ccbbd688807c046a9116ce94072a1113d3b9
王志敏 <t-wangzhimin@qq.com>
2022-11-02 21:45:53 -0500

    feat: First commit-Create springboot framework

---
+++ src/test/java/com/smu/databaseteamproject/DatabaseTeamProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smu.databaseteamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseTeamProject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
