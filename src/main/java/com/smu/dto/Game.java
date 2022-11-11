1a1debdfed8818d241e763b4f3df6f99583846e6
henry <henryperfectest@gmail.com>
2022-11-11 00:24:38 -0600

    Second updated Game class on 11/11/2022

--- src/main/java/com/smu/dto/Game.java
+++ src/main/java/com/smu/dto/Game.java
@@ -1,14 +1,29 @@
 package com.smu.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
-import lombok.experimental.Accessors;
+
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
 
 /**
  * Game
  *
+ * @author S.Z 11/11/22
  * @author T.W 11/3/22
  */
 @Data
 public class Game {
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @NotEmpty
+    private LocalDate gameDate;
+    @NotEmpty
+    private String location;
+    @NotEmpty
+    private String homeTeam;
+    @NotEmpty
+    private String visitTeam;
+    @NotEmpty
+    private String score;
 
 }
